3eb0b61143d5aade90ca5e005bfe997034cd2103
lakshyamodi832 <lakshyaymodi@gmail.com>
2026-01-26 23:16:52 +0530

    rupee

---
+++ rupee.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+class Paise;
+
+class Rupees{
+    double rs;
+    public:
+    Rupees(){
+
+    }
+    Rupees(double r){
+        rs = r;
+    }
+    void disp(){
+        cout<<rs<<" rupees"<<endl;
+    }
+    double getRs(){
+        return rs;
+    }
+    operator Paise();
+};
+
+class Paise{
+    int ps;
+    public:
+    Paise(){
+
+    }
+    Paise(int p){
+        ps = p;
+    }
+    int getPs(){
+        return ps;
+    }
+    void _disp(){
+        cout<<ps<<" paise"<<endl;
+    }
+    operator Rupees(){
+        return Rupees(ps/100.0);
+    }
+};
+
+Rupees::operator Paise(){
+    return Paise(rs*100);
+}
+
+int main(){
+ 
+    Rupees r1(1.2);
+
+    Paise p1(640);
+
+    Rupees r2(p1);
+
+    Paise p2(r1);
+
+    r1.disp();
+
+    r2.disp();
+
+    p1._disp();
+
+    p2._disp();
+
+    return 0;
+} 
